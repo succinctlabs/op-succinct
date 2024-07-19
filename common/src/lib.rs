d824bf3963147caad1001db78f2a2abe6ace686b
Zach Obront <zobront@gmail.com>
2024-07-19 11:20:33 -0500

    try to debug this import tree

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -7,5 +7,7 @@ pub use boot::BootInfoWithoutRollupConfig;
 mod executor;
 pub use executor::block_on;
 
+#[cfg(feature = "io")]
 mod data_fetcher;
+#[cfg(feature = "io")]
 pub use data_fetcher::SP1KonaDataFetcher;
