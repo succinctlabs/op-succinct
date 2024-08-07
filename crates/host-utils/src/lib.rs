80ab3bfc479a5283a939d145cbea1e5a0e7081ea
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-08-07 15:50:05 -0700

    add

--- crates/host-utils/src/lib.rs
+++ crates/host-utils/src/lib.rs
@@ -52,8 +52,8 @@ pub fn get_sp1_stdin(host_cli: &HostCli) -> Result<SP1Stdin> {
 
     let mut serializer = CompositeSerializer::new(
         AlignedSerializer::new(AlignedVec::new()),
-        // TODO: This value is hardcoded to minimum for this block.
-        // Figure out how to compute it so it works on all blocks.
+        // TODO: This value corresponds to the size of the space needed to
+        // serialize the KV store. We should compute this dynamically.
         HeapScratch::<33554432>::new(),
         SharedSerializeMap::new(),
     );
