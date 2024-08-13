fbf7f44e2153cf5e82b4046987ff2627163069f8
Ubuntu <ubuntu@op-mainnet-archive-1.maas>
2024-08-13 19:31:04 +0000

    add max block range per span proof

--- op_proposer.sh
+++ op_proposer.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Run op-proposer
+
+# Currently, configured to generate a proof once per minute.
+
 /usr/local/bin/op-proposer \
     --poll-interval=${POLL_INTERVAL:-12s} \
     --rollup-rpc=${ROLLUP_RPC} \
@@ -9,6 +12,7 @@
     --l1-eth-rpc=${L1_ETH_RPC} \
     --beacon-rpc=${BEACON_RPC} \
     --l2-chain-id=${L2_CHAIN_ID} \
-    --max-concurrent-proof-requests=${MAX_CONCURRENT_PROOF_REQUESTS} \
-    --db-path=/usr/local/bin/proofs.db \
-    --kona-server-url=${KONA_SERVER_URL}
+    --max-concurrent-proof-requests=${MAX_CONCURRENT_PROOF_REQUESTS:-3} \
+    --db-path=/usr/local/bin/proofs.db \ 
+    --kona-server-url=${KONA_SERVER_URL} \
+    --max-block-range-per-span-proof=${MAX_BLOCK_RANGE_PER_SPAN_PROOF:-30}
