ded2fa2031ca7899617917ff5973d9c5d1564d66
Ubuntu <ubuntu@op-mainnet-archive-1.maas>
2024-08-28 17:23:10 +0000

    keccak

--- crates/client-utils/src/oracle/mod.rs
+++ crates/client-utils/src/oracle/mod.rs
@@ -93,7 +93,7 @@ impl InMemoryOracle {
                 PreimageKeyType::Keccak256 => {
                     let derived_key =
                         PreimageKey::new(keccak256(value).into(), PreimageKeyType::Keccak256);
-                    assert_eq!(key, derived_key, "zkvm keccak256 constraint failed!");
+                    assert_eq!(key, derived_key, "zkvm keccak constraint failed!");
                 }
                 PreimageKeyType::GlobalGeneric => {
                     unimplemented!();
