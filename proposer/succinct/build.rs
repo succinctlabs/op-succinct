580a9ea046587af6fb373618d1dc05d290953640
ratankaliani <ratan@succinct.xyz>
2024-09-05 21:52:47 +0000

    got compilation, working, still issues

--- proposer/succinct/build.rs
+++ proposer/succinct/build.rs
@@ -1,6 +1,6 @@
 use std::process::Command;
 
-// use sp1_build::{build_program_with_args, BuildArgs};
+use sp1_build::{build_program_with_args, BuildArgs};
 
 /// Build a native program.
 fn build_native_program(program: &str) {
@@ -41,17 +41,17 @@ fn build_native_host_runner() {
     println!("cargo:warning=native_host_runner built with release profile",);
 }
 
-// /// Build a program for the zkVM.
-// fn build_zkvm_program(program: &str) {
-//     build_program_with_args(
-//         &format!("../../programs/{}", program),
-//         BuildArgs {
-//             elf_name: format!("{}-elf", program),
-//             // docker: true,
-//             ..Default::default()
-//         },
-//     );
-// }
+/// Build a program for the zkVM.
+fn build_zkvm_program(program: &str) {
+    build_program_with_args(
+        &format!("../../programs/{}", program),
+        BuildArgs {
+            elf_name: format!("{}-elf", program),
+            // docker: true,
+            ..Default::default()
+        },
+    );
+}
 
 fn main() {
     let programs = vec!["range"];
@@ -60,7 +60,7 @@ fn main() {
         // Note: Don't comment this out, because the Docker program depends on the native program
         // for range being built.
         build_native_program(program);
-        // build_zkvm_program(program);
+        build_zkvm_program(program);
     }
 
     // build_zkvm_program("aggregation");
