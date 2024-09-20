c8daee5332e5593618373277f115f84b7bd8bb51
ratankaliani <ratan@succinct.xyz>
2024-09-20 20:52:04 +0000

    update elfs

--- programs/aggregation/src/main.rs
+++ programs/aggregation/src/main.rs
@@ -16,7 +16,7 @@ use sha2::{Digest, Sha256};
 ///
 /// Whenever the multi-block program changes, you will need to update this.
 const MULTI_BLOCK_PROGRAM_VKEY_DIGEST: [u32; 8] =
-    [1433563224, 915959902, 1739327614, 1467493203, 514223451, 768124479, 1632058379, 944956804];
+    [1118246686, 876333357, 1751535833, 1253784834, 30755083, 1698492772, 1080266404, 1468343823];
 
 pub fn main() {
     // Read in the public values corresponding to each multi-block proof.
