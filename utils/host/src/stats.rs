d5401f19de06351c90956c674bbbda7ba2ee2ffd
ratankaliani <ratan@succinct.xyz>
2024-10-03 23:11:03 +0000

    add

--- utils/host/src/stats.rs
+++ utils/host/src/stats.rs
@@ -60,7 +60,7 @@ impl fmt::Display for ExecutionStats {
         write_stat(f, "Batch End", self.batch_end)?;
         write_stat(
             f,
-            "Witness Generation Duration (seconds)",
+            "Witness Generation (seconds)",
             self.witness_generation_time_sec,
         )?;
         write_stat(
