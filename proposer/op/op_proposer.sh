73b40ae994ed798fb2d7ea63d38e7ba186d5659a
ratankaliani <ratan@succinct.xyz>
2024-10-08 03:01:21 +0000

    fix

--- proposer/op/op_proposer.sh
+++ proposer/op/op_proposer.sh
@@ -12,8 +12,8 @@
     --private-key=${PRIVATE_KEY} \
     --l1-eth-rpc=${L1_RPC} \
     --beacon-rpc=${L1_BEACON_RPC} \
-    --max-concurrent-proof-requests=${MAX_CONCURRENT_PROOF_REQUESTS:-10} \
+    --max-concurrent-proof-requests=${MAX_CONCURRENT_PROOF_REQUESTS:-5} \
     --db-path=${DB_PATH:-/usr/local/bin/dbdata} \
     --op-succinct-server-url=${OP_SUCCINCT_SERVER_URL:-http://op-succinct-server:3000} \
-    --max-block-range-per-span-proof=${MAX_BLOCK_RANGE_PER_SPAN_PROOF:-20} \
+    --max-block-range-per-span-proof=${MAX_BLOCK_RANGE_PER_SPAN_PROOF:-500} \
     --use-cached-db=${USE_CACHED_DB:-false}
