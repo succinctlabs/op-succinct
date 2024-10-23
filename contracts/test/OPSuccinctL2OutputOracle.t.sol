cfa251c6be1b33266ea0996ff6a46b6a1859ab08
ratankaliani <ratan@succinct.xyz>
2024-10-23 19:49:07 +0000

    feat: update test

--- contracts/test/OPSuccinctL2OutputOracle.t.sol
+++ contracts/test/OPSuccinctL2OutputOracle.t.sol
@@ -7,11 +7,11 @@ import {OPSuccinctL2OutputOracle} from "../src/OPSuccinctL2OutputOracle.sol";
 
 contract OPSuccinctL2OutputOracleTest is Test, Utils {
     // The checkpointed block number for the proof.
-    uint256 checkpointedL1BlockNum = 6919874;
-    bytes32 claimedOutputRoot = 0x918d98ae75c4aa89ac9098a85dc18e0777bf073d1943d991e1f1f54a1f92450c;
-    uint256 claimedL2BlockNum = 3381733;
+    uint256 checkpointedL1BlockNum = 6931062;
+    bytes32 claimedOutputRoot = 0xf5ef905ba2c0e598c2f5274177700f3dfe37f66db15e8957e63d0732b0e611b8;
+    uint256 claimedL2BlockNum = 3677705;
     bytes proof =
-        hex"91ff06f30532505692c10b9a952d3392017a67fc9c5997246150379c74ef11b28087b1912a2e591c0b3d5a80ed7b224a7cecfc0b882dca09ad9bcc4ac26a76dfbecc2e0f1aac3b85587c867bed8eea3402f10d381f5df4ddb54249dd6c29d967a458cb6c2b09d136197641e2894503a52dc095bcb3c783c6171ed0bbc77d05a967e8d15e1f31fe6af7cf3d669f1e62b5c1c66404baa7c7717ac57fdd803e0938bab3369822889c5b8eb4ab50dfbcdc6f99ac0d3b3290876a17d36604cb5c88ec5c8780c10ef7d49135d9b835e8f49308b86576cbf4d3e3b955807d6c592c0c42817099b617bea8309f49d31ba6630f1b73ba10f051942a43d22cd118612b6d3c252f15b6";
+        hex"91ff06f303ed1bf4b5dbf52b2dd7201cb9675afd59200464ef55cff01d113ca54d96b52c2689d0a64c90eb674d1cb9119e4f4fde54d9414d056112df7bf01066b86ee5e410d4d6a93c26c287e1c010bf03fcc0ebfaa6ae294650bba1bf177271c96911771624e73cf6192e3f1a5ac0bd7943f5921df5c22e1c2661a40c33a40b70e9f8d6164ab1e3e1abd666c19aae2012ec389a295e9ce148f781a81363685da83b32390785840f77691e93d734863d283a05497f8a8621dd1dc5e410b6bef0ed9ce53422a8b41ebdbc7e82202fafa1dd5a0fcc458932f76390f9d1f1fbf4134cf68dec06bf5b5b1c0cde47bd89198a52e7b92c634da6dadcf59efa6b78d51273e3316d";
 
     // The owner of the L2OO.
     address OWNER = 0xDEd0000E32f8F40414d3ab3a830f735a3553E18e;
@@ -27,7 +27,7 @@ contract OPSuccinctL2OutputOracleTest is Test, Utils {
     // TODO: Once we have a new contract deployed with the new L2OO interface, we should use that here.
     function testOPSuccinctL2OOFork() public {
         console.log(block.number);
-        l2oo = OPSuccinctL2OutputOracle(0xd9979DD3cbE74C46fdD8bDB122775Fc7D0DF0BCE);
+        l2oo = OPSuccinctL2OutputOracle(0x83EBf366f868784c91d49fBEe67651F7a3de74C5);
         l2oo.checkpointBlockHash(checkpointedL1BlockNum);
         vm.prank(OWNER);
         l2oo.proposeL2Output(claimedOutputRoot, claimedL2BlockNum, checkpointedL1BlockNum, proof);
