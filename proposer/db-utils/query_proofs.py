b21b82e2c9a324a6f6a09dd4ee95a7167878c020
ratankaliani <ratan@succinct.xyz>
2024-12-15 03:27:23 +0000

    keep split range basic

--- proposer/db-utils/query_proofs.py
+++ proposer/db-utils/query_proofs.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 import time
 import sys
-
+from datetime import datetime
 # Types of proofs
 class ProofType(Enum):
     SPAN = "SPAN"
@@ -28,13 +28,14 @@ class ProofRequest:
     request_added_time: int
     prover_request_id: str
     proof_request_time: int
+    last_updated_time: int
     l1_block_number: int
     l1_block_hash: str
     proof: bytes
 
     def __init__(self, id: int, type: ProofType, start_block: int, end_block: int,
                  status: ProofStatus, request_added_time: int, prover_request_id: str,
-                 proof_request_time: int, l1_block_number: int, l1_block_hash: str, proof: bytes):
+                 proof_request_time: int, last_updated_time: int, l1_block_number: int, l1_block_hash: str, proof: bytes):
         self.id = id
         self.type = type
         self.start_block = start_block
@@ -43,6 +44,7 @@ class ProofRequest:
         self.request_added_time = request_added_time
         self.prover_request_id = prover_request_id
         self.proof_request_time = proof_request_time
+        self.last_updated_time = last_updated_time
         self.l1_block_number = l1_block_number
         self.l1_block_hash = l1_block_hash
         self.proof = proof
@@ -58,9 +60,10 @@ def convert_to_proof_request(result):
         request_added_time=result[5],
         prover_request_id=result[6],
         proof_request_time=result[7],
-        l1_block_number=result[8],
-        l1_block_hash=result[9],
-        proof=result[10]
+        last_updated_time=result[8],
+        l1_block_number=result[9],
+        l1_block_hash=result[10],
+        proof=result[11]
     )
 
 # Queries proofs of a specific type from the database
@@ -113,8 +116,8 @@ if __name__ == "__main__":
     print("-" * 50)
     span_proofs = query_span_proofs(db_path)
     for proof in span_proofs:
-        if proof.status is not ProofStatus.UNREQ:
-            print(f"Request ID: {proof.id}, Type: {proof.type}, Start Block: {proof.start_block}, End Block: {proof.end_block}, Status: {proof.status}, Prover Request ID: {proof.prover_request_id}, Request Added Time: {proof.request_added_time}, Proof Request Time: {proof.proof_request_time}")
+        if proof.status is ProofStatus.FAILED:
+            print(f"Request ID: {proof.id}, Type: {proof.type}, Start Block: {proof.start_block}, End Block: {proof.end_block}, Status: {proof.status}, Prover Request ID: {proof.prover_request_id}, Request Added Time: {datetime.fromtimestamp(proof.request_added_time).strftime('%Y-%m-%d %H:%M:%S UTC')}, Proof Request Time: {datetime.fromtimestamp(proof.proof_request_time).strftime('%Y-%m-%d %H:%M:%S UTC') if proof.proof_request_time else 'None'}, Last Updated Time: {datetime.fromtimestamp(proof.last_updated_time).strftime('%Y-%m-%d %H:%M:%S UTC')}")
     print("-" * 50)
 
     # Query for aggregation proofs
