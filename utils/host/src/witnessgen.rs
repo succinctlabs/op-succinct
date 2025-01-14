70a1783ba4e9ba3f9f3e89fd9640710b80df632a
Ratan Kaliani <ratankaliani@berkeley.edu>
2025-01-14 15:26:38 -0800

    add

--- utils/host/src/witnessgen.rs
+++ utils/host/src/witnessgen.rs
@@ -56,8 +56,7 @@ pub fn convert_host_cli_to_args(host_cli: &HostCli) -> Vec<String> {
     args
 }
 
-/// Default timeout for witness generation.
-/// TODO: Long proof generation, set to 12H. Decide if we should remove this timeout.
+/// Set to 12 hours to allow for long witness generation.
 pub const WITNESSGEN_TIMEOUT: Duration = Duration::from_secs(60 * 60 * 12);
 
 struct WitnessGenProcess {
