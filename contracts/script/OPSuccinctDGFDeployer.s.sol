c122a23c16a4d202954e79b6b5d3c43fdbd77f9a
fakedev9999 <taehoon@succinct.xyz>
2025-02-11 00:22:07 -0800

    feat(contracts): use `DisputeGameFactory`
    Removes unnecessary `OPSuccinctDisputeGameFactory`

--- contracts/script/OPSuccinctDGFDeployer.s.sol
+++ contracts/script/OPSuccinctDGFDeployer.s.sol
@@ -4,10 +4,13 @@ pragma solidity ^0.8.15;
 import {Script} from "forge-std/Script.sol";
 import {OPSuccinctL2OutputOracle} from "../src/validity/OPSuccinctL2OutputOracle.sol";
 import {OPSuccinctDisputeGame} from "../src/validity/OPSuccinctDisputeGame.sol";
-import {OPSuccinctDisputeGameFactory} from "../src/validity/OPSuccinctDisputeGameFactory.sol";
+import {DisputeGameFactory} from "@optimism/src/dispute/DisputeGameFactory.sol";
 import {Utils} from "../test/helpers/Utils.sol";
 import {Proxy} from "@optimism/src/universal/Proxy.sol";
 import {console} from "forge-std/console.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {GameType} from "src/dispute/lib/Types.sol";
+import {IDisputeGame} from "src/dispute/interfaces/IDisputeGame.sol";
 
 contract OPSuccinctDFGDeployer is Script, Utils {
     function run() public returns (address) {
@@ -19,7 +22,22 @@ contract OPSuccinctDFGDeployer is Script, Utils {
 
         // Initialize the dispute game based on the existing L2OO_ADDRESS.
         OPSuccinctDisputeGame game = new OPSuccinctDisputeGame(address(l2OutputOracleProxy));
-        OPSuccinctDisputeGameFactory gameFactory = new OPSuccinctDisputeGameFactory(msg.sender, address(game));
+
+        // Deploy the factory implementation
+        DisputeGameFactory factoryImpl = new DisputeGameFactory();
+
+        // Deploy factory proxy
+        ERC1967Proxy factoryProxy = new ERC1967Proxy(
+            address(factoryImpl), abi.encodeWithSelector(DisputeGameFactory.initialize.selector, msg.sender)
+        );
+
+        // Cast the factory proxy to the factory contract
+        DisputeGameFactory gameFactory = DisputeGameFactory(address(factoryProxy));
+
+        // Set the init bond and implementation for the game type
+        GameType gameType = GameType.wrap(uint32(6));
+        gameFactory.setInitBond(gameType, 0.01 ether);
+        gameFactory.setImplementation(gameType, IDisputeGame(address(game)));
 
         vm.stopBroadcast();
 
