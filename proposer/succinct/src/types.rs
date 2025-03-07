7c78dd87c48c6ab12eeda0e3e3a007406f86a3ff
Ratan Kaliani <ratankaliani@berkeley.edu>
2025-03-07 12:58:07 -0800

    chore: add sp1 gas

--- proposer/succinct/src/types.rs
+++ proposer/succinct/src/types.rs
@@ -131,8 +131,7 @@ impl RequestExecutionStatistics {
 
         Self {
             total_instruction_cycles: execution_report.total_instruction_count(),
-            // TODO: Add this when SP1 prover gas is available.
-            total_sp1_gas: 0,
+            total_sp1_gas: execution_report.gas.unwrap_or(0),
             block_execution_cycles: get_cycles("block-execution"),
             oracle_verify_cycles: get_cycles("oracle-verify"),
             derivation_cycles: get_cycles("payload-derivation"),
