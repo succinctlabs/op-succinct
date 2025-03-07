0d51b56906240c5e3ba028622552650586761f00
Ubuntu <ubuntu@large-op-succinct-dev.maas>
2025-03-07 23:28:12 +0000

    feat: bump to 720 blocks (12 hours) which is the op reorg window

--- utils/host/src/fetcher.rs
+++ utils/host/src/fetcher.rs
@@ -914,7 +914,7 @@ impl OPSuccinctDataFetcher {
                 11155420 => 10,
                 10 => 10,
                 8453 => 10,
-                _ => 360,
+                _ => 720,
             };
 
             // Get L1 head.
