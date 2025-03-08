b4f4dd32cb17109c7865a201b2fa25eb35af14b7
Ratan Kaliani <ratankaliani@berkeley.edu>
2025-03-07 21:39:22 -0800

    chore: forget memory zkvm

--- utils/build/src/lib.rs
+++ utils/build/src/lib.rs
@@ -4,43 +4,43 @@ use sp1_build::{build_program_with_args, BuildArgs};
 /// Build all the native programs and the native host runner. Optional flag to build the zkVM
 /// programs.
 pub fn build_all() {
-    // let metadata = cargo_metadata::MetadataCommand::new()
-    //     .exec()
-    //     .expect("Failed to get cargo metadata");
-    // build_program_with_args(
-    //     &format!("{}/{}", metadata.workspace_root.join("programs"), "aggregation"),
-    //     BuildArgs {
-    //         elf_name: Some("aggregation-elf".to_string()),
-    //         output_directory: Some("../../elf".to_string()),
-    //         docker: true,
-    //         tag: "v4.1.2".to_string(),
-    //         workspace_directory: Some("../../".to_string()),
-    //         ..Default::default()
-    //     },
-    // );
+    let metadata = cargo_metadata::MetadataCommand::new()
+        .exec()
+        .expect("Failed to get cargo metadata");
+    build_program_with_args(
+        &format!("{}/{}", metadata.workspace_root.join("programs"), "aggregation"),
+        BuildArgs {
+            elf_name: Some("aggregation-elf".to_string()),
+            output_directory: Some("../../elf".to_string()),
+            docker: true,
+            tag: "v4.1.3".to_string(),
+            workspace_directory: Some("../../".to_string()),
+            ..Default::default()
+        },
+    );
 
-    // build_program_with_args(
-    //     &format!("{}/{}", metadata.workspace_root.join("programs"), "range"),
-    //     BuildArgs {
-    //         elf_name: Some("range-elf-bump".to_string()),
-    //         output_directory: Some("../../elf".to_string()),
-    //         docker: true,
-    //         tag: "v4.1.2".to_string(),
-    //         workspace_directory: Some("../../".to_string()),
-    //         ..Default::default()
-    //     },
-    // );
+    build_program_with_args(
+        &format!("{}/{}", metadata.workspace_root.join("programs"), "range"),
+        BuildArgs {
+            elf_name: Some("range-elf-bump".to_string()),
+            output_directory: Some("../../elf".to_string()),
+            docker: true,
+            tag: "v4.1.3".to_string(),
+            workspace_directory: Some("../../".to_string()),
+            ..Default::default()
+        },
+    );
 
-    // build_program_with_args(
-    //     &format!("{}/{}", metadata.workspace_root.join("programs"), "range"),
-    //     BuildArgs {
-    //         elf_name: Some("range-elf-embedded".to_string()),
-    //         output_directory: Some("../../elf".to_string()),
-    //         docker: true,
-    //         tag: "v4.1.2".to_string(),
-    //         workspace_directory: Some("../../".to_string()),
-    //         features: vec!["embedded".to_string()],
-    //         ..Default::default()
-    //     },
-    // );
+    build_program_with_args(
+        &format!("{}/{}", metadata.workspace_root.join("programs"), "range"),
+        BuildArgs {
+            elf_name: Some("range-elf-embedded".to_string()),
+            output_directory: Some("../../elf".to_string()),
+            docker: true,
+            tag: "v4.1.3".to_string(),
+            workspace_directory: Some("../../".to_string()),
+            features: vec!["embedded".to_string()],
+            ..Default::default()
+        },
+    );
 }
