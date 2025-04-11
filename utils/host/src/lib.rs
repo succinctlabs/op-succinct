62aaf17661f6c00f2dcf00f7c554775b6bf99d71
Ratan Kaliani <ratankaliani@berkeley.edu>
2025-04-10 19:24:40 -0700

    remove daconfig

--- utils/host/src/lib.rs
+++ utils/host/src/lib.rs
@@ -8,9 +8,6 @@ pub use contract::*;
 pub use proof::*;
 pub mod metrics;
 
-use clap::{Parser, ValueEnum};
-use strum_macros::EnumString;
-
 pub const RANGE_ELF_BUMP: &[u8] = include_bytes!("../../../elf/range-elf-bump");
 pub const RANGE_ELF_EMBEDDED: &[u8] = include_bytes!("../../../elf/range-elf-embedded");
 pub const AGGREGATION_ELF: &[u8] = include_bytes!("../../../elf/aggregation-elf");
@@ -20,17 +17,6 @@ pub const CELESTIA_RANGE_ELF_EMBEDDED: &[u8] =
 
 // TODO: Update to EigenDA Range ELF Embedded
 pub const EIGENDA_RANGE_ELF_EMBEDDED: &[u8] = include_bytes!("../../../elf/range-elf-embedded");
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Parser, ValueEnum, EnumString)]
-/// The configuration for the DA provider.
-pub enum DAConfig {
-    /// The default DA configuration.
-    Default,
-    /// The Celestia DA configuration.
-    Celestia,
-    /// The EigenDA DA configuration.
-    EigenDA,
-}
 
 /// Get the range ELF depending on the feature flag.
 pub fn get_range_elf_embedded() -> &'static [u8] {
