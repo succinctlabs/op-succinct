97dc4afe28356f3bbb43ff1e90abd5cb65fc8860
L Kuklinek <lkuklinek@polygon.technology>
2025-04-22 13:03:06 +0100

    refactor: move zkvm ELFs into a separate crate
    This makes it easier for third party software to use the zkvm ELF binary
    data (e.g. for the purpose of calculating the verification keys) without
    having to involve the heavy dependencies of `op-succinct-host-utils`.

--- utils/host/src/lib.rs
+++ utils/host/src/lib.rs
@@ -9,14 +9,10 @@ pub use proof::*;
 pub mod metrics;
 pub mod witness_generation;
 
-pub const RANGE_ELF_BUMP: &[u8] = include_bytes!("../../../elf/range-elf-bump");
-pub const RANGE_ELF_EMBEDDED: &[u8] = include_bytes!("../../../elf/range-elf-embedded");
-pub const AGGREGATION_ELF: &[u8] = include_bytes!("../../../elf/aggregation-elf");
-pub const CELESTIA_RANGE_ELF_EMBEDDED: &[u8] =
-    include_bytes!("../../../elf/celestia-range-elf-embedded");
-
-// TODO: Update to EigenDA Range ELF Embedded
-pub const EIGENDA_RANGE_ELF_EMBEDDED: &[u8] = include_bytes!("../../../elf/range-elf-embedded");
+pub use op_succinct_elfs::{
+    AGGREGATION_ELF, CELESTIA_RANGE_ELF_EMBEDDED, EIGENDA_RANGE_ELF_EMBEDDED, RANGE_ELF_BUMP,
+    RANGE_ELF_EMBEDDED,
+};
 
 /// Get the range ELF depending on the feature flag.
 pub fn get_range_elf_embedded() -> &'static [u8] {
