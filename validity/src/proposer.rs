52c2f02ee0a32b8577458e7977f1233fc4a1d84e
Ratan Kaliani <ratankaliani@berkeley.edu>
2025-05-01 13:42:25 -0700

    feat: add transactionwrapperstruct

--- validity/src/proposer.rs
+++ validity/src/proposer.rs
@@ -5,7 +5,7 @@ use crate::{
     ValidityGauge,
 };
 use alloy_consensus::{TxEnvelope, TypedTransaction};
-use alloy_eips::BlockId;
+use alloy_eips::{BlockId, Decodable2718};
 use alloy_network::{EthereumWallet, TransactionBuilder, TransactionBuilder4844};
 use alloy_primitives::{Address, Bytes, B256, U256};
 use alloy_provider::{
@@ -23,6 +23,7 @@ use op_succinct_host_utils::{
     OPSuccinctL2OutputOracle::OPSuccinctL2OutputOracleInstance as OPSuccinctL2OOContract,
     AGGREGATION_ELF,
 };
+use serde::{Deserialize, Serialize};
 use sp1_sdk::{
     network::proto::network::{ExecutionStatus, FulfillmentStatus},
     HashableKey, NetworkProver, Prover, ProverClient, SP1Proof, SP1ProofWithPublicValues,
@@ -55,7 +56,6 @@ where
     requester_config: RequesterConfig,
     proof_requester: Arc<OPSuccinctProofRequester<H>>,
     tasks: Arc<Mutex<TaskMap>>,
-    provider: P,
 }
 
 /// 5 L1 confirmations (1 minute)
@@ -169,7 +169,6 @@ where
             requester_config,
             proof_requester,
             tasks: Arc::new(Mutex::new(HashMap::new())),
-            provider,
         };
         Ok(proposer)
     }
@@ -1234,9 +1233,20 @@ where
             // Sign the transaction request using the Web3Signer.
             let web3_provider = ProviderBuilder::new().network::<N>().connect_http(signer_url);
             let signer = Web3Signer::new(web3_provider.clone(), signer_address);
-            let signed_tx = signer.sign_and_decode(filled_tx.as_builder().unwrap().clone()).await?;
 
-            Ok(provider.send_tx_envelope(signed_tx).await?)
+            let tx = filled_tx.as_builder().unwrap().clone();
+
+            // NOTE: This is a hack because there is not a "data" field on the TransactionRequest.
+            // `eth_signTransaction` expects a "data" field with the calldata.
+            // TODO: Once alloy fixes this, we can remove this wrapper.
+            let wrapper = TransactionRequestWrapper::<N>::new(tx);
+
+            let raw: Bytes =
+                signer.provider().client().request("eth_signTransaction", (wrapper,)).await?;
+
+            let tx_envelope = N::TxEnvelope::decode_2718(&mut raw.as_ref()).unwrap();
+
+            Ok(provider.send_tx_envelope(tx_envelope).await?)
         }
         ProposerSigner::LocalSigner(private_key) => {
             let provider = ProviderBuilder::new()
@@ -1252,5 +1262,67 @@ where
 
             Ok(provider.send_tx_envelope(filled_tx.as_envelope().unwrap().clone()).await?)
         }
+    }
+}
+
+/// A wrapper around `TransactionRequest` that adds a data field as bytes.
+///
+/// This is a hack because there is not a "data" field on the TransactionRequest.
+/// `eth_signTransaction` expects a "data" field with the calldata.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct TransactionRequestWrapper<N: Network> {
+    /// The underlying transaction request
+    #[serde(flatten)]
+    pub tx: N::TransactionRequest,
+    /// The transaction data as bytes
+    pub data: Option<Bytes>,
+}
+
+impl<N: Network> TransactionRequestWrapper<N> {
+    /// Create a new wrapper around a transaction request
+    pub fn new(tx: N::TransactionRequest) -> Self {
+        let data = tx.input().cloned();
+        Self { tx, data }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use alloy_network::Ethereum;
+    use alloy_primitives::address;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_sign_transaction_request() {
+        let proposer_signer = ProposerSigner::Web3Signer(
+            "http://localhost:9000".parse().unwrap(),
+            "0x9b3F173823E944d183D532ed236Ee3B83Ef15E1d".parse().unwrap(),
+        );
+
+        let provider = ProviderBuilder::new()
+            .network::<Ethereum>()
+            .connect_http("http://localhost:8545".parse().unwrap());
+
+        let l2oo_contract = OPSuccinctL2OOContract::new(
+            address!("0xDafA1019F21AB8B27b319B1085f93673F02A69B7"),
+            provider.clone(),
+        );
+
+        let latest_header = provider.get_block(BlockId::latest()).await.unwrap().unwrap();
+
+        let transaction_request = l2oo_contract
+            .checkpointBlockHash(U256::from(latest_header.header.number))
+            .into_transaction_request();
+
+        let signed_tx = sign_transaction_request_inner::<Ethereum>(
+            proposer_signer,
+            "http://localhost:8545".parse().unwrap(),
+            transaction_request,
+        )
+        .await
+        .unwrap();
+
+        println!("Signed transaction receipt: {:?}", signed_tx.get_receipt().await.unwrap());
     }
 }
