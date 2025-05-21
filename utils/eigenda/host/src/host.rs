cf654f2e202f34a579eb7d02d36bb1cf645c82c9
fakedev9999 <taehoon@succinct.xyz>
2025-05-21 16:31:32 +0000

    feat: support eigenda

---
+++ utils/eigenda/host/src/host.rs
@@ -0,0 +1,76 @@
+use std::sync::Arc;
+
+use alloy_eips::BlockId;
+use alloy_primitives::B256;
+use anyhow::Result;
+use async_trait::async_trait;
+use hokulea_host_bin::cfg::SingleChainHostWithEigenDA;
+use hokulea_proof::eigenda_blob_witness::EigenDABlobWitnessData;
+use op_succinct_eigenda_client_utils::executor::EigenDAWitnessExecutor;
+use op_succinct_host_utils::{fetcher::OPSuccinctDataFetcher, host::OPSuccinctHost};
+
+use crate::witness_generator::EigenDAWitnessGenerator;
+
+#[derive(Clone)]
+pub struct EigenDAOPSuccinctHost {
+    pub fetcher: Arc<OPSuccinctDataFetcher>,
+    pub witness_generator: Arc<EigenDAWitnessGenerator>,
+}
+
+#[async_trait]
+impl OPSuccinctHost for EigenDAOPSuccinctHost {
+    type Args = SingleChainHostWithEigenDA;
+    type WitnessGenerator = EigenDAWitnessGenerator;
+
+    fn witness_generator(&self) -> &Self::WitnessGenerator {
+        &self.witness_generator
+    }
+
+    async fn fetch(
+        &self,
+        l2_start_block: u64,
+        l2_end_block: u64,
+        l1_head_hash: Option<B256>,
+        safe_db_fallback: Option<bool>,
+    ) -> Result<SingleChainHostWithEigenDA> {
+        let host = self
+            .fetcher
+            .get_host_args(
+                l2_start_block,
+                l2_end_block,
+                l1_head_hash,
+                safe_db_fallback.expect("`safe_db_fallback` must be set"),
+            )
+            .await?;
+
+        let eigenda_proxy_address = std::env::var("EIGENDA_PROXY_ADDRESS").ok();
+        Ok(SingleChainHostWithEigenDA { kona_cfg: host, eigenda_proxy_address, verbose: 1 })
+    }
+
+    fn get_l1_head_hash(&self, args: &Self::Args) -> Option<B256> {
+        Some(args.kona_cfg.l1_head)
+    }
+
+    async fn get_finalized_l2_block_number(
+        &self,
+        fetcher: &OPSuccinctDataFetcher,
+        _: u64,
+    ) -> Result<Option<u64>> {
+        let finalized_l2_block_number = fetcher.get_l2_header(BlockId::finalized()).await?;
+        Ok(Some(finalized_l2_block_number.number))
+    }
+}
+
+impl EigenDAOPSuccinctHost {
+    pub fn new(
+        fetcher: Arc<OPSuccinctDataFetcher>,
+        eigenda_witness: EigenDABlobWitnessData,
+    ) -> Self {
+        Self {
+            fetcher,
+            witness_generator: Arc::new(EigenDAWitnessGenerator {
+                executor: EigenDAWitnessExecutor::new(eigenda_witness),
+            }),
+        }
+    }
+}
