f71a70a3563045cb4578a1f149dfbb18045f6345
Farhad Shabani <farhad.shabani@gmail.com>
2025-11-11 13:56:54 -0800

    test: some anch cases

--- fault-proof/tests/sync.rs
+++ fault-proof/tests/sync.rs
@@ -2,7 +2,7 @@ mod common;
 
 #[cfg(feature = "e2e")]
 mod sync {
-    use std::collections::HashMap;
+    use std::{collections::HashMap, time::Duration};
 
     use crate::common::{
         constants::{
@@ -18,6 +18,8 @@ mod sync {
     use rand::Rng;
     use rstest::rstest;
 
+    const M: u32 = u32::MAX;
+
     async fn setup() -> Result<(
         TestEnvironment,
         OPSuccinctProposer<fault_proof::L1Provider, impl OPSuccinctHost + Clone>,
@@ -30,8 +32,6 @@ mod sync {
         Ok((env, proposer, init_bond))
     }
 
-    const M: u32 = u32::MAX;
-
     // Naming guide used in case names:
     // - "*_noanch_*": anchored game absent.
     // - "*_anch_*": anchored game present.
@@ -41,7 +41,7 @@ mod sync {
     #[case::noanch_single_game_default_interval(1, &[], &[M], &[], Some(0), 1)] // Branch: M->0, Blocks: 0->1
     #[case::noanch_single_game_large_interval(1, &[], &[M], &[10], Some(0), 10)] // Branch: M->0, Blocks: 0->10
     #[case::noanch_two_games_same_branch(2, &[], &[M, 0], &[], Some(1), 2)] // Branch: M->0->1, Blocks: 0->1->2
-    #[case::noanch_two_games_same_parent_diff_intervals(2, &[], &[M, M], &[1, 2], Some(1), 2)] // Branch: M->0->1, Blocks: 0->1->3
+    #[case::noanch_two_games_same_parent_diff_intervals(2, &[], &[M, M], &[1, 2], Some(1), 2)] // Branches: M->0, M->1, Blocks: 0->1, 0->2
     #[case::noanch_three_games_same_branch(3,&[], &[M, 0, 1], &[], Some(2), 3)] // Branch: M->0->1->2, Blocks: 0->1->2->3
     #[case::noanch_three_games_same_parent_diff_intervals_1(3,&[], &[M, M, M], &[1, 2, 3], Some(2), 3)] // Branches: M->0, M->1, M->2, Blocks: 0->1, 0->2, 0->3
     #[case::noanch_three_games_same_parent_diff_intervals_2(3,&[], &[M, M, M], &[1, 3, 2], Some(1), 3)] // Branches: M->0, M->1, M->2, Blocks: 0->1, 0->3, 0->2
@@ -51,10 +51,13 @@ mod sync {
     #[case::noanch_five_games_two_branches(5, &[], &[M, 0, 1, 0, 3], &[1, 1, 1, 2, 2], Some(4), 5)] // Branches: M->0->1->2, 0->3->4, Blocks: 0->1->2->3 and 1->3->5
     #[case::noanch_five_games_three_branches(5, &[], &[M, 0, 1, 0, 0], &[1, 1, 1, 4, 3], Some(3), 5)] // Branches: M->0->1->2, 0->3, 0->4, Blocks: 0->1->2->3, 1->5, 1->4
     #[case::anch_single_game_default_interval(1, &[0], &[M], &[], Some(0), 1)]
+    #[case::anch_two_games_same_branch(2, &[0, 1], &[M, 0], &[], Some(1), 2)]
+    #[case::anch_two_games_same_parent_diff_intervals(2, &[0], &[M, M], &[1, 2], Some(0), 1)]
+    #[case::anch_five_games_two_branches(5, &[0, 1], &[M, 0, 1, 0, 3], &[1, 1, 1, 2, 2], Some(2), 3)]
     #[tokio::test]
     async fn test_sync_state_happy_paths(
         #[case] num_games: usize,
-        #[case] finalized_ids: &[usize],
+        #[case] anchor_ids: &[usize],
         #[case] parent_ids: &[u32],
         #[case] intervals: &[u64],
         #[case] expected_canonical_head_index: Option<u64>,
@@ -76,15 +79,12 @@ mod sync {
             let (index, address) = env.last_game_info().await?;
             tracing::info!("Created game {index} with parent {cur_parent_id}");
 
-            if finalized_ids.contains(&i) {
-                let finality_wait = DISPUTE_GAME_FINALITY_DELAY_SECONDS +
-                    MAX_CHALLENGE_DURATION +
-                    MAX_PROVE_DURATION +
-                    60;
-                env.warp_time(finality_wait).await?;
+            if anchor_ids.contains(&i) {
+                env.warp_time(MAX_CHALLENGE_DURATION + 60).await?;
                 env.resolve_game(address).await?;
+                env.warp_time(DISPUTE_GAME_FINALITY_DELAY_SECONDS + 60).await?;
                 env.set_anchor_state(address).await?;
-                tracing::info!("Warped time by {finality_wait} seconds and finalized game {i}");
+                tracing::info!("Anchor game set to index {index}");
             }
 
             // Determine the starting block for the next game
@@ -105,10 +105,10 @@ mod sync {
         let snapshot = proposer.state_snapshot().await;
         let expected_canonical_head_index =
             expected_canonical_head_index.map(|idx| U256::from(idx));
-        let expected_anchor_index = if finalized_ids.is_empty() {
+        let expected_anchor_index = if anchor_ids.is_empty() {
             None
         } else {
-            Some(U256::from(*finalized_ids.iter().max().unwrap()))
+            Some(U256::from(*anchor_ids.iter().max().unwrap()))
         };
 
         assert_eq!(snapshot.anchor_index, expected_anchor_index, "Anchor index should match");
