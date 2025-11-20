5678df902f7fede874394cf492ac347f2a6a20ed
Farhad Shabani <farhad.shabani@gmail.com>
2025-11-19 22:26:27 -0800

    refactor: supervisor wirings

--- tests/presets/validity.go
+++ tests/presets/validity.go
@@ -15,10 +15,10 @@ import (
 const DefaultL1ID = 900
 const DefaultL2ID = 901
 
-func WithSuccinctValidityProposer(dest *sysgo.DefaultMinimalSystemIDs) stack.CommonOption {
+func WithSuccinctValidityProposer(dest *sysgo.DefaultSingleChainInteropSystemIDs) stack.CommonOption {
 	l1ChainID := eth.ChainIDFromUInt64(DefaultL1ID)
 	l2ChainID := eth.ChainIDFromUInt64(DefaultL2ID)
-	ids := sysgo.NewDefaultMinimalSystemIDs(l1ChainID, l2ChainID)
+	ids := sysgo.NewDefaultSingleChainInteropSystemIDs(l1ChainID, l2ChainID)
 
 	opt := stack.Combine[*sysgo.Orchestrator]()
 	opt.Add(stack.BeforeDeploy(func(o *sysgo.Orchestrator) {
@@ -38,30 +38,31 @@ func WithSuccinctValidityProposer(dest *sysgo.DefaultMinimalSystemIDs) stack.Com
 		),
 		sysgo.WithDeployerOptions(
 			func(_ devtest.P, _ devkeys.Keys, builder intentbuilder.Builder) {
-				builder.WithGlobalOverride("l2BlockTime", uint64(2))
+				builder.WithGlobalOverride("l2BlockTime", uint64(1))
 				builder.WithL1ContractsLocator(artifacts.MustNewFileLocator(artifactsPath))
 				builder.WithL2ContractsLocator(artifacts.MustNewFileLocator(artifactsPath))
 			},
-			sysgo.WithSequencingWindow(10),
-			sysgo.WithFinalizationPeriodSeconds(10),
 			sysgo.WithCommons(ids.L1.ChainID()),
-			sysgo.WithPrefundedL2(ids.L1.ChainID(), ids.L2.ChainID()),
+			sysgo.WithPrefundedL2(ids.L1.ChainID(), ids.L2A.ChainID()),
+			sysgo.WithSequencingWindow(2),
 		),
 	)
 
 	opt.Add(sysgo.WithL1Nodes(ids.L1EL, ids.L1CL))
 
-	opt.Add(sysgo.WithL2ELNode(ids.L2EL))
-	opt.Add(sysgo.WithL2CLNode(ids.L2CL, ids.L1CL, ids.L1EL, ids.L2EL, sysgo.L2CLSequencer()))
+	opt.Add(sysgo.WithSupervisor(ids.Supervisor, ids.Cluster, ids.L1EL))
+	opt.Add(sysgo.WithL2ELNode(ids.L2AEL, sysgo.L2ELWithSupervisor(ids.Supervisor)))
+	opt.Add(sysgo.WithL2CLNode(ids.L2ACL, ids.L1CL, ids.L1EL, ids.L2AEL, sysgo.L2CLSequencer(), sysgo.L2CLIndexing()))
+	opt.Add(sysgo.WithManagedBySupervisor(ids.L2ACL, ids.Supervisor))
 
-	opt.Add(sysgo.WithBatcher(ids.L2Batcher, ids.L1EL, ids.L2CL, ids.L2EL))
-	opt.Add(sysgo.WithTestSequencer(ids.TestSequencer, ids.L1CL, ids.L2CL, ids.L1EL, ids.L2EL))
+	opt.Add(sysgo.WithBatcher(ids.L2ABatcher, ids.L1EL, ids.L2ACL, ids.L2AEL))
+	opt.Add(sysgo.WithTestSequencer(ids.TestSequencer, ids.L1CL, ids.L2ACL, ids.L1EL, ids.L2AEL))
+	opt.Add(sysgo.WithFaucets([]stack.L1ELNodeID{ids.L1EL}, []stack.L2ELNodeID{ids.L2AEL}))
 
-	opt.Add(sysgo.WithDeploySP1MockVerifier(ids.L1EL, l2ChainID))
-	opt.Add(sysgo.WithDeployOpSuccinctL2OutputOracle(ids.L1CL, ids.L1EL, ids.L2CL, ids.L2EL))
-	opt.Add(sysgo.WithSuccinctValidityProposer(ids.L2Proposer, ids.L1CL, ids.L1EL, ids.L2CL, ids.L2EL))
+	opt.Add(sysgo.WithSuperDeploySP1MockVerifier(ids.L1EL, l2ChainID))
+	opt.Add(sysgo.WithSuperDeployOpSuccinctL2OutputOracle(ids.L1CL, ids.L1EL, ids.L2ACL, ids.L2AEL))
+	opt.Add(sysgo.WithSuperSuccinctValidityProposer(ids.L2AProposer, ids.L1CL, ids.L1EL, ids.L2ACL, ids.L2AEL))
 
-	opt.Add(sysgo.WithFaucets([]stack.L1ELNodeID{ids.L1EL}, []stack.L2ELNodeID{ids.L2EL}))
 	opt.Add(sysgo.WithL2MetricsDashboard())
 	opt.Add(stack.Finally(func(orch *sysgo.Orchestrator) {
 		*dest = ids
