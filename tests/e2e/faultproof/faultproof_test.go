0ef68dfb4ab50c3b85f23bbdda72400f7fcfd671
Farhad Shabani <farhad.shabani@gmail.com>
2025-11-24 12:10:35 -0800

    test: detect first game created

--- tests/e2e/faultproof/faultproof_test.go
+++ tests/e2e/faultproof/faultproof_test.go
@@ -1,7 +1,9 @@
 package e2e
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/ethereum-optimism/optimism/op-devstack/devtest"
 	"github.com/ethereum-optimism/optimism/op-devstack/presets"
@@ -32,4 +34,33 @@ func TestFaultProofProposer_L2DgfDeployedAndUp(gt *testing.T) {
 	require.NoError(t, err, "failed to get game count from DGF")
 	t.Logger().Info("Dispute Game Count:", "count", gameCount)
 	require.Equal(t, uint64(0), gameCount, "expected zero dispute games initially")
+}
+
+func TestFaultProofProposer_DetectsFirstGameCreated(gt *testing.T) {
+	t := devtest.SerialT(gt)
+	sys := presets.NewMinimalWithProposer(t)
+
+	ctx, cancel := context.WithTimeout(t.Ctx(), 5*time.Minute)
+	defer cancel()
+
+	dgfAddr := sys.L2Chain.Escape().Deployment().DisputeGameFactoryProxyAddr()
+	t.Logger().Info("Dispute Game Factory Address:", "address", dgfAddr.Hex())
+	dgf, err := utils.NewDgfClient(sys.L1EL.EthClient(), dgfAddr)
+
+	require.NoError(t, err, "failed to create DGF client")
+
+	for {
+		gameCount, err := dgf.GameCount(t.Ctx())
+		require.NoError(t, err, "failed to get game count from DGF")
+
+		if gameCount >= 1 {
+			break
+		}
+
+		select {
+		case <-ctx.Done():
+			t.Errorf("timeout waiting for dispute game to be created")
+		case <-time.After(time.Second):
+		}
+	}
 }
