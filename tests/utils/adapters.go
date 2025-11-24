1744fe5536c4e01d15ba09197d5cd3b3fba55d57
Farhad Shabani <farhad.shabani@gmail.com>
2025-11-24 07:51:02 -0800

    refactor: resuable call function in adapters

--- tests/utils/adapters.go
+++ tests/utils/adapters.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"math/big"
 	"strings"
@@ -15,11 +14,16 @@ import (
 	opsbind "github.com/succinctlabs/op-succinct/bindings"
 )
 
+// contractClient is a generic Ethereum contract client.
+type contractClient struct {
+	client apis.EthClient
+	addr   common.Address
+	abi    abi.ABI
+}
+
 // L2OOClient is a client for interacting with the SuccinctL2OutputOracle contract.
 type L2OOClient struct {
-	client   apis.EthClient
-	l2ooAddr common.Address
-	abi      abi.ABI
+	contractClient
 }
 
 // NewL2OOClient creates a new L2OOClient.
@@ -30,68 +34,86 @@ func NewL2OOClient(client apis.EthClient, l2ooAddr common.Address) (*L2OOClient,
 	}
 
 	return &L2OOClient{
-		client:   client,
-		l2ooAddr: l2ooAddr,
-		abi:      parsedABI,
+		contractClient: contractClient{
+			client: client,
+			addr:   l2ooAddr,
+			abi:    parsedABI,
+		},
 	}, nil
 }
 
 // LatestBlockNumber fetches the latest L2 block number from the contract.
 func (l2oo *L2OOClient) LatestBlockNumber(ctx context.Context) (uint64, error) {
-	data, err := l2oo.abi.Pack("latestBlockNumber")
-	if err != nil {
-		return 0, err
-	}
-
-	callMsg := ethereum.CallMsg{
-		To:   &l2oo.l2ooAddr,
-		Data: data,
-	}
-
-	raw, err := l2oo.client.Call(ctx, callMsg, rpc.LatestBlockNumber)
-	if err != nil {
-		return 0, err
-	}
-
-	outs, err := l2oo.abi.Unpack("latestBlockNumber", raw)
-	if err != nil {
-		return 0, err
-	}
-
-	if len(outs) != 1 {
-		return 0, errors.New("unexpected number of outputs from latestBlockNumber")
-	}
-
-	latestBlock := outs[0].(*big.Int)
-	return latestBlock.Uint64(), nil
+	return call(ctx, l2oo.contractClient, "latestBlockNumber", asUint64)
 }
 
 // NextBlockNumber fetches the next L2 block number to be submitted by the proposer.
 func (l2oo *L2OOClient) NextBlockNumber(ctx context.Context) (uint64, error) {
-	data, err := l2oo.abi.Pack("nextBlockNumber")
+	return call(ctx, l2oo.contractClient, "nextBlockNumber", asUint64)
+}
+
+// DgfClient is a client for interacting with the DisputeGameFactory contract.
+type DgfClient struct {
+	contractClient
+}
+
+// NewDgfClient creates a new DgfClient.
+func NewDgfClient(client apis.EthClient, DfgAddr common.Address) (*DgfClient, error) {
+	parsedABI, err := abi.JSON(strings.NewReader(opsbind.DisputeGameFactoryMetaData.ABI))
 	if err != nil {
-		return 0, fmt.Errorf("pack nextBlockNumber call: %w", err)
+		return nil, fmt.Errorf("parse Dispute Game Factory ABI: %w", err)
+	}
+
+	return &DgfClient{
+		contractClient: contractClient{
+			client: client,
+			addr:   DfgAddr,
+			abi:    parsedABI,
+		},
+	}, nil
+}
+
+// GameCount fetches the number of dispute games created.
+func (dfg *DgfClient) GameCount(ctx context.Context) (uint64, error) {
+	return call(ctx, dfg.contractClient, "gameCount", asUint64)
+}
+
+// call is a generic function to call a contract method and convert the output.
+func call[R any](ctx context.Context, c contractClient, method string, convert func(string, []any) (R, error)) (R, error) {
+	var zero R
+
+	data, err := c.abi.Pack(method)
+	if err != nil {
+		return zero, fmt.Errorf("pack %s call: %w", method, err)
 	}
 
 	callMsg := ethereum.CallMsg{
-		To:   &l2oo.l2ooAddr,
+		To:   &c.addr,
 		Data: data,
 	}
 
-	raw, err := l2oo.client.Call(ctx, callMsg, rpc.LatestBlockNumber)
+	raw, err := c.client.Call(ctx, callMsg, rpc.LatestBlockNumber)
 	if err != nil {
-		return 0, err
+		return zero, fmt.Errorf("call %s: %w", method, err)
 	}
 
-	outs, err := l2oo.abi.Unpack("nextBlockNumber", raw)
+	outs, err := c.abi.Unpack(method, raw)
 	if err != nil {
-		return 0, err
+		return zero, fmt.Errorf("unpack %s: %w", method, err)
 	}
 
-	if len(outs) != 1 {
-		return 0, err
+	return convert(method, outs)
+}
+
+// asUint64 converts the output of a contract call to uint64.
+func asUint64(method string, outputs []any) (uint64, error) {
+	if len(outputs) != 1 {
+		return 0, fmt.Errorf("unexpected number of outputs from %s", method)
 	}
 
-	latestBlock := outs[0].(*big.Int)
-	return latestBlock.Uint64(), nil
+	value, ok := outputs[0].(*big.Int)
+	if !ok {
+		return 0, fmt.Errorf("unexpected output type from %s: %T", method, outputs[0])
+	}
+	return value.Uint64(), nil
 }
