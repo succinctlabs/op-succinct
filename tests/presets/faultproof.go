9140220a6a15932ff21540ac460d557870702af2
Farhad Shabani <farhad.shabani@gmail.com>
2025-11-25 21:37:36 -0800

    fix: update /optimism rev

--- tests/presets/faultproof.go
+++ tests/presets/faultproof.go
@@ -9,7 +9,7 @@ import (
 func WithSuccinctFaultProofProposer(dest *sysgo.DefaultSingleChainInteropSystemIDs) stack.CommonOption {
 	return withSuccinctPreset(dest, func(opt *stack.CombinedOption[*sysgo.Orchestrator], ids sysgo.DefaultSingleChainInteropSystemIDs, l2ChainID eth.ChainID) {
 		opt.Add(sysgo.WithSuperDeploySP1MockVerifier(ids.L1EL, l2ChainID))
-		opt.Add(sysgo.WithSuperDeployOPSuccinctFaultDisputeGame(ids.L1CL, ids.L1EL, ids.L2ACL, ids.L2AEL, sysgo.WithFdgL2StaringBlockNumber(1)))
+		opt.Add(sysgo.WithSuperDeployOPSuccinctFaultDisputeGame(ids.L1CL, ids.L1EL, ids.L2ACL, ids.L2AEL, sysgo.WithFdgL2StartingBlockNumber(1)))
 		opt.Add(sysgo.WithSuperSuccinctFaultProofProposer(ids.L2AProposer, ids.L1CL, ids.L1EL, ids.L2ACL, ids.L2AEL,
 			sysgo.WithFdgProposalIntervalInBlocks(5),
 			sysgo.WithFdgFetchInterval(1)))
