4deb973df47e55e47c85df64e7acd49150fabf2f
Farhad Shabani <farhad.shabani@gmail.com>
2025-11-25 18:56:14 -0800

    test: check first game's parentId

--- tests/e2e/faultproof/faultproof_test.go
+++ tests/e2e/faultproof/faultproof_test.go
@@ -2,13 +2,13 @@ package e2e
 
 import (
 	"context"
+	"math"
 	"testing"
 	"time"
 
 	"github.com/ethereum-optimism/optimism/op-devstack/devtest"
 	"github.com/ethereum-optimism/optimism/op-devstack/presets"
 	"github.com/ethereum-optimism/optimism/op-devstack/sysgo"
-	"github.com/stretchr/testify/require"
 	opspresets "github.com/succinctlabs/op-succinct/presets"
 	"github.com/succinctlabs/op-succinct/utils"
 )
@@ -23,45 +23,47 @@ func TestMain(m *testing.M) {
 func TestFaultProofProposer_L2DgfDeployedAndUp(gt *testing.T) {
 	t := devtest.SerialT(gt)
 	sys := presets.NewMinimalWithProposer(t)
+	require := t.Require()
+	logger := t.Logger()
 
 	dgfAddr := sys.L2Chain.Escape().Deployment().DisputeGameFactoryProxyAddr()
-	t.Logger().Info("Dispute Game Factory Address:", "address", dgfAddr.Hex())
+	logger.Info("Dispute Game Factory Address:", "address", dgfAddr.Hex())
 
 	dgf, err := utils.NewDgfClient(sys.L1EL.EthClient(), dgfAddr)
-	require.NoError(t, err, "failed to create DGF client")
+	require.NoError(err, "failed to create DGF client")
 
 	gameCount, err := dgf.GameCount(t.Ctx())
-	require.NoError(t, err, "failed to get game count from DGF")
-	t.Logger().Info("Dispute Game Count:", "count", gameCount)
+	require.NoError(err, "failed to get game count from DGF")
+	logger.Info("Dispute Game Count:", "count", gameCount)
 	require.Equal(t, uint64(0), gameCount, "expected zero dispute games initially")
 }
 
 func TestFaultProofProposer_DetectsFirstGameCreated(gt *testing.T) {
 	t := devtest.SerialT(gt)
 	sys := presets.NewMinimalWithProposer(t)
+	require := t.Require()
+	logger := t.Logger()
 
 	ctx, cancel := context.WithTimeout(t.Ctx(), 5*time.Minute)
 	defer cancel()
 
 	dgfAddr := sys.L2Chain.Escape().Deployment().DisputeGameFactoryProxyAddr()
-	t.Logger().Info("Dispute Game Factory Address:", "address", dgfAddr.Hex())
+	logger.Info("Dispute Game Factory Address:", "address", dgfAddr.Hex())
 	dgf, err := utils.NewDgfClient(sys.L1EL.EthClient(), dgfAddr)
 
-	require.NoError(t, err, "failed to create DGF client")
+	require.NoError(err, "failed to create Dispute Game Factory client")
 
-	for {
-		gameCount, err := dgf.GameCount(t.Ctx())
-		require.NoError(t, err, "failed to get game count from DGF")
+	utils.WaitForGameCount(ctx, t, dgf, 1)
 
-		if gameCount >= 1 {
-			break
-		}
+	game, err := dgf.GameAtIndex(ctx, 0)
+	require.NoError(err, "failed to get first game from factory")
+	logger.Info("First game created", "gameType", game.GameType, "timestamp", game.Timestamp, "proxy", game.Proxy.Hex())
 
-		select {
-		case <-ctx.Done():
-			t.Errorf("timeout waiting for dispute game to be created")
-			t.FailNow()
-		case <-time.After(time.Second):
-		}
-	}
+	fdg, err := utils.NewFdgClient(sys.L1EL.EthClient(), game.Proxy)
+	require.NoError( err, "failed to create Fault Dispute Game client")
+
+	parentIdx, err := fdg.ParentIndex(ctx)
+	require.NoError( err, "failed to read parentIndex")
+	t.Logger().Info("Fault dispute game parent index", "parentIndex", parentIdx)
+	require.Equal(uint32(math.MaxUint32), parentIdx, "unexpected parent index")
 }
