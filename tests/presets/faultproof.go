5babe97c19163fdeeaa77e178440b24747239f27
Farhad Shabani <farhad.shabani@gmail.com>
2025-11-26 20:00:29 -0800

    feat: add FastFinality preset

--- tests/presets/faultproof.go
+++ tests/presets/faultproof.go
@@ -6,12 +6,23 @@ import (
 	"github.com/ethereum-optimism/optimism/op-service/eth"
 )
 
-func WithSuccinctFaultProofProposer(dest *sysgo.DefaultSingleChainInteropSystemIDs) stack.CommonOption {
+func WithDefaultSuccinctFPProposer(dest *sysgo.DefaultSingleChainInteropSystemIDs) stack.CommonOption {
 	return withSuccinctPreset(dest, func(opt *stack.CombinedOption[*sysgo.Orchestrator], ids sysgo.DefaultSingleChainInteropSystemIDs, l2ChainID eth.ChainID) {
 		opt.Add(sysgo.WithSuperDeploySP1MockVerifier(ids.L1EL, l2ChainID))
 		opt.Add(sysgo.WithSuperDeployOPSuccinctFaultDisputeGame(ids.L1CL, ids.L1EL, ids.L2ACL, ids.L2AEL, sysgo.WithFdgL2StartingBlockNumber(1)))
 		opt.Add(sysgo.WithSuperSuccinctFaultProofProposer(ids.L2AProposer, ids.L1CL, ids.L1EL, ids.L2ACL, ids.L2AEL,
-			sysgo.WithFdgProposalIntervalInBlocks(5),
-			sysgo.WithFdgFetchInterval(1)))
+			sysgo.WithFPProposalIntervalInBlocks(5),
+			sysgo.WithFPFetchInterval(1)))
+	})
+}
+
+func WithSuccinctFPProposerFastFinality(dest *sysgo.DefaultSingleChainInteropSystemIDs) stack.CommonOption {
+	return withSuccinctPreset(dest, func(opt *stack.CombinedOption[*sysgo.Orchestrator], ids sysgo.DefaultSingleChainInteropSystemIDs, l2ChainID eth.ChainID) {
+		opt.Add(sysgo.WithSuperDeploySP1MockVerifier(ids.L1EL, l2ChainID))
+		opt.Add(sysgo.WithSuperDeployOPSuccinctFaultDisputeGame(ids.L1CL, ids.L1EL, ids.L2ACL, ids.L2AEL, sysgo.WithFdgL2StartingBlockNumber(1)))
+		opt.Add(sysgo.WithSuperSuccinctFaultProofProposer(ids.L2AProposer, ids.L1CL, ids.L1EL, ids.L2ACL, ids.L2AEL,
+			sysgo.WithFPProposalIntervalInBlocks(5),
+			sysgo.WithFPFetchInterval(1),
+			sysgo.WithFPFastFinalityMode(true)))
 	})
 }
