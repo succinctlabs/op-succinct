3aa2f28df0cfa38b61823ca62b26ec8f3ed7f3cd
Farhad Shabani <farhad.shabani@gmail.com>
2025-12-02 07:16:43 -0800

    chore: tune params so run on CI

--- tests/presets/faultproof.go
+++ tests/presets/faultproof.go
@@ -21,12 +21,12 @@ func WithSuccinctFPProposerFastFinality(dest *sysgo.DefaultSingleChainInteropSys
 		opt.Add(sysgo.WithSuperDeploySP1MockVerifier(ids.L1EL, l2ChainID))
 		opt.Add(sysgo.WithSuperDeployOPSuccinctFaultDisputeGame(ids.L1CL, ids.L1EL, ids.L2ACL, ids.L2AEL, sysgo.WithFdgL2StartingBlockNumber(1)))
 		opt.Add(sysgo.WithSuperSuccinctFaultProofProposer(ids.L2AProposer, ids.L1CL, ids.L1EL, ids.L2ACL, ids.L2AEL,
-			sysgo.WithFPProposalIntervalInBlocks(20),
+			sysgo.WithFPProposalIntervalInBlocks(40),
 			sysgo.WithFPFetchInterval(1),
 			sysgo.WithFPRangeSplitCount(16),
 			sysgo.WithFPMaxConcurrentRangeProofs(16),
 			sysgo.WithFPFastFinalityMode(true),
-			// Set to 8 to allow more game creation tasks while proving is still in progress, enabling faster test completion.
-			sysgo.WithFPFastFinalityProvingLimit(8)))
+			// Set to 16 to allow more game creation tasks while proving is still in progress, enabling faster test completion.
+			sysgo.WithFPFastFinalityProvingLimit(16)))
 	})
 }
