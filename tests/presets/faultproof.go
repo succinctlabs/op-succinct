5dfd0b9df39c9458c9f3964c91c78fa6b87a6160
Farhad Shabani <farhad.shabani@gmail.com>
2025-12-09 20:27:06 -0800

    fix: dropped configs in fp presets

--- tests/presets/faultproof.go
+++ tests/presets/faultproof.go
@@ -52,7 +52,11 @@ func FastFinalityFaultProofConfig() FaultProofConfig {
 func WithSuccinctFPProposer(dest *sysgo.DefaultSingleChainInteropSystemIDs, cfg FaultProofConfig) stack.CommonOption {
 	return withSuccinctPreset(dest, func(opt *stack.CombinedOption[*sysgo.Orchestrator], ids sysgo.DefaultSingleChainInteropSystemIDs, l2ChainID eth.ChainID) {
 		opt.Add(sysgo.WithSuperDeploySP1MockVerifier(ids.L1EL, l2ChainID))
-		opt.Add(sysgo.WithSuperDeployOPSuccinctFaultDisputeGame(ids.L1CL, ids.L1EL, ids.L2ACL, ids.L2AEL, sysgo.WithFdgL2StartingBlockNumber(1)))
+		opt.Add(sysgo.WithSuperDeployOPSuccinctFaultDisputeGame(ids.L1CL, ids.L1EL, ids.L2ACL, ids.L2AEL,
+			sysgo.WithFdgL2StartingBlockNumber(1),
+			sysgo.WithFdgMaxChallengeDuration(cfg.MaxChallengeDuration),
+			sysgo.WithFdgMaxProveDuration(cfg.MaxProveDuration),
+			sysgo.WithFdgDisputeGameFinalityDelaySecs(cfg.DisputeGameFinalityDelaySecs)))
 
 		proposerOpts := []sysgo.FaultProofProposerOption{
 			sysgo.WithFPProposalIntervalInBlocks(cfg.ProposalIntervalInBlocks),
