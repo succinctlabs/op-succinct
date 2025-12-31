b82753b46a92841a179609dcacc16c31cb012686
Farhad Shabani <farhad.shabani@gmail.com>
2025-12-31 07:51:40 -0800

    fix: rebasing issue

--- fault-proof/tests/common/process.rs
+++ fault-proof/tests/common/process.rs
@@ -46,7 +46,7 @@ pub async fn new_proposer(
         use_kms_requester: false,
         range_split_count: RangeSplitCount::one(),
         max_concurrent_range_proofs: NonZero::<usize>::MIN,
-        backup_file: None,
+        backup_file,
         proof_provider: ProofProviderConfig {
             timeout: 14400, // 4 hours
             network_calls_timeout: 15,
